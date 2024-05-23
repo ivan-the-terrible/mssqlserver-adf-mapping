93c3ec9c24bd40ae8474d538d012d25dadbbc371
ivan-the-terrible <ivanchwalik@hotmail.com>
2024-05-23 13:58:10 -0400

    fix: removed test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-from anytree import Node, Resolver
-
-test = Node("test", children=(Node("child1"), Node("child2")))
-
-test.children += (Node("child1"),)
-print(test.children)
-# resolver = Resolver("name")
-# resolver.get(test, "child1")
-# print(resolver.get(test, "child1"))

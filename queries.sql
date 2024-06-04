f4dc7f4d72aeb2f95f88260634a38a4c0deccd2a
ivan-the-terrible <ivanchwalik@hotmail.com>
2024-06-04 14:12:22 -0400

    feat: order values by name for alphabetical viewing

--- queries.sql
+++ queries.sql
@@ -1,18 +1,18 @@
 --https://database.guide/4-ways-to-list-all-views-in-a-sql-server-database/
 
 -- Tables
-SELECT SCHEMA_NAME(schema_id) + '.' + name FROM sys.tables ORDER BY schema_id
+SELECT SCHEMA_NAME(schema_id) + '.' + name FROM sys.tables ORDER BY schema_id, name
 
 -- Views
 SELECT SCHEMA_NAME(schema_id) + '.' + name, definition FROM sys.views v
 INNER JOIN sys.sql_modules m
 ON v.object_id = m.object_id
 WHERE v.schema_id != 4 -- Exclude sys schema
-ORDER BY schema_id
+ORDER BY schema_id, name
 
 -- Stored Procedures
 SELECT SCHEMA_NAME(schema_id) + '.' + name, definition FROM sys.procedures p
 INNER JOIN sys.sql_modules m
 ON p.object_id = m.object_id
 WHERE p.schema_id != 4 -- Exclude sys schema
-ORDER BY schema_id
+ORDER BY schema_id, name
